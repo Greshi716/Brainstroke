e13a58e38ffdbce036e9adf641cf2cdefaedc97a
Greshi716 <101010572+Greshi716@users.noreply.github.com>
2023-04-08 09:39:41 +0530

    Update datavisualizer.py

--- cloudProject/data_visualizing/datavisualizer.py
+++ cloudProject/data_visualizing/datavisualizer.py
@@ -1,17 +1,17 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
+# import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plt
 
-class datavisualizer:
-    def shape(self, data):
-        return data.shape
-    def describe(self, data):
-        return data.describe()
-    def histogram(self, data):
-        data.hist(figsize=(10,10))
-        plt.savefig('graphs/histogram.PNG')
-    def boxplot(self, data):
-        lst = pd.Series.tolist(data.columns)
-        plt.figure(figsize=(50, 50))
-        data.boxplot(lst)
-        plt.savefig('graphs/boxplot.PNG')
+# class datavisualizer:
+#     def shape(self, data):
+#         return data.shape
+#     def describe(self, data):
+#         return data.describe()
+#     def histogram(self, data):
+#         data.hist(figsize=(10,10))
+#         plt.savefig('graphs/histogram.PNG')
+#     def boxplot(self, data):
+#         lst = pd.Series.tolist(data.columns)
+#         plt.figure(figsize=(50, 50))
+#         data.boxplot(lst)
+#         plt.savefig('graphs/boxplot.PNG')
